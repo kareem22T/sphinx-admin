d84ba23b653dea14b8feb4a998e523b7d57e5df3
Kareem Mohamed <kotbekareem74@gmail.com>
2025-01-07 16:12:22 +0200

    edits

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -9,8 +9,17 @@ class Setting extends Model
 {
     use HasFactory;
     protected $fillable = [
-        "key",
-        "data"
+        'hotels',
+        'tours',
+        'ad_image',
+        'ad_title_en',
+        'ad_title_ar',
+        'ad_description_en',
+        'ad_description_ar',
+        'ad2_image',
+        'ad2_title_en',
+        'ad2_title_ar',
+        'ad2_description_en',
+        'ad2_description_ar',
     ];
-
 }
