1ee80b043478a0d80a08af4e910694fd84633ea0
Kareem Mohamed <kotbekareem74@gmail.com>
2025-02-22 07:38:32 +0200

    edit

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -12,11 +12,19 @@ class CouponController extends Controller
     {
         $request->validate([
             'coupon_code' => 'required|string',
-            'tour_id' => 'nullable|integer',
-            'hotel_id' => 'nullable|integer',
+            'tour_id' => 'nullable|string',
+            'hotel_id' => 'nullable|string',
         ]);
 
         $coupon = Coupon::where('coupon_code', $request->coupon_code)
+            ->where(function ($query) use ($request) {
+                if ($request->filled('tour_id')) {
+                    $query->whereJsonContains('tours', $request->tour_id);
+                }
+                if ($request->filled('hotel_id')) {
+                    $query->whereJsonContains('hotels', $request->hotel_id);
+                }
+            })
             ->whereDate('start_date', '<=', Carbon::now())
             ->whereDate('end_date', '>=', Carbon::now())
             ->first();
