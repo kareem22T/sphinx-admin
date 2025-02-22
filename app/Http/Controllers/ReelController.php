7e995aa188f2a38bd9b8664b1c0945f3ce0c50d5
Kareem Mohamed <kotbekareem74@gmail.com>
2025-02-22 06:39:18 +0200

    edit

--- app/Http/Controllers/ReelController.php
+++ app/Http/Controllers/ReelController.php
@@ -10,31 +10,7 @@ class ReelController extends Controller
     // Fetch the latest reels
     public function index()
     {
-        $reels = Reel::latest()->take(10)->get();
+        $reels = Reel::latest()->get();
         return response()->json($reels);
-    }
-
-    // Fetch the next reel
-    public function next($currentReelId)
-    {
-        $nextReel = Reel::where('id', '>', $currentReelId)->orderBy('id', 'asc')->first();
-
-        if ($nextReel) {
-            return response()->json($nextReel);
-        }
-
-        return response()->json(['message' => 'No more reels available'], 404);
-    }
-
-    // Fetch the previous reel
-    public function prev($currentReelId)
-    {
-        $prevReel = Reel::where('id', '<', $currentReelId)->orderBy('id', 'desc')->first();
-
-        if ($prevReel) {
-            return response()->json($prevReel);
-        }
-
-        return response()->json(['message' => 'No more reels available'], 404);
     }
 }
